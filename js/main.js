df6856d2c885a12381ada47983876ee2fe5599db
Alex <alex@snapwiz.com>
2016-06-23 16:23:42 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var Glider = angular.module("Glider", []);
+
+Glider.directive("scroll", function ($window) {
+    return function(scope, element, attrs) {
+        angular.element($window).bind("scroll", function() {
+             if (this.pageYOffset >= 100) {
+                 scope.boolChangeClass = true;
+             } else {
+                 scope.boolChangeClass = false;
+             }
+            scope.$apply();
+        });
+    };
+})
+
+Glider.controller("header", function($scope) {
+
+	$scope.MenuActive = false;
+  $scope.MegamenuActive = false;
+  $scope.activeMenu = function() {
+    $scope.MenuActive = !$scope.MenuActive;
+  }
+
+  $scope.activeMegaMenu = function() {
+    $scope.MegamenuActive = !$scope.MegamenuActive;
+  }
+});
